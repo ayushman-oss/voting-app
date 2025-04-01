64c2a75d269d4831b072bea2af00b0c7b645ab58
ayushman1044 <ayushman1044@gmail.com>
2025-04-01 13:53:12 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -50,8 +50,6 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
 
 module.exports = app;
