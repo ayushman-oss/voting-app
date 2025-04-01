75727a4640ff5820f96bc1fd54587b848e19b931
Ayushman Behera <78735337+ayushman-oss@users.noreply.github.com>
2025-04-01 21:04:33 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ document.getElementById("login-form").addEventListener("submit", async function 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("/auth/login", {
+    const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
