8bc581c14ff57112bd6d7ab6daf00c1afe7ebb8e
ayushman1044 <ayushman1044@gmail.com>
2025-04-02 18:18:26 +0000

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -16,13 +16,16 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   console.log("Incoming request URL:", req.url); // Log the request URL
-  const { url } = req;
-  if (url.startsWith("/admin")) {
-    await adminHandler(req, res);
-  } else if (url.startsWith("/voter")) {
-    await voterHandler(req, res);
-  } else if (url.startsWith("/verify")) {
-    await verifyHandler(req, res);
+  const { url, method } = req;
+  if (method === 'POST' && url === '/voter/verify') {
+      await voterHandler(req, res);
+  }
+   else if (url.startsWith("/admin")) {
+      await adminHandler(req, res);
+   } else if (url.startsWith("/voter")) {
+      await voterHandler(req, res);
+   } else if (url.startsWith("/verify")) {
+      await verifyHandler(req, res);
   } else if (url.startsWith("/auth")) {
     await authHandler(req, res);
   } else if (url.startsWith("/voted")) {
